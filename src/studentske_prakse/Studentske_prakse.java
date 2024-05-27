7a3a0b28c92e5b192baf796b297c15882d88fe69
PetarPosrkaca <posrkaca003@gmail.com>
2024-05-27 23:52:49 +0200

    Uspesno povezana baza

--- src/studentske_prakse/Studentske_prakse.java
+++ src/studentske_prakse/Studentske_prakse.java
@@ -3,7 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package studentske_prakse;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 /**
  *
  * @author student2
@@ -11,7 +15,28 @@ package studentske_prakse;
 public class Studentske_prakse {
 
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        String url = "jdbc:mysql://localhost:3306/studentske_prakse";
+        String user = "root";
+        String password = "";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Uspesno uspostavljena veza");
+
+            String query = "SELECT * FROM smer";
+            try (Statement statement = connection.createStatement();
+                 ResultSet rs = statement.executeQuery(query)) {
+
+                while (rs.next()) {
+                    int ID = rs.getInt("ID");
+                    String ime = rs.getString("naziv");
+                    System.out.println(ime);
+                }
+            } catch (SQLException e) {
+                System.err.println("Error executing query: " + e.getMessage());
+            }
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database: " + e.getMessage());
+        }
     
+    }
 }
